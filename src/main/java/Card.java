93ac5ba0b219378a1e42776361dcac6049081da1
Stevecv <37953488+Stevecv@users.noreply.github.com>
2023-12-08 10:12:53 +0000

    Update Card.java
    Fixed accidental mis-ordering due to numbers and letters

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,45 +1,61 @@
 public class Card {
-    public Character card;
-    public int value;
-    public char hexValue;
+  public Character card;
+  public int value;
+  public char alphaValue;
 
-    public Card(Character card) {
-        this.card = card;
-        this.value = getValue();
-        this.hexValue = getValueHex();
+  public Card(Character card) {
+    this.card = card;
+    this.value = getValue();
+    this.alphaValue = getValueAlpha();
+  }
+
+  private Integer getValue() {
+    switch (card) {
+      case '2', '3', '4', '5', '6', '7', '8', '9':
+        return Integer.valueOf(card);
+      case 'T':
+        return 10;
+      case 'J':
+        return 11;
+      case 'Q':
+        return 12;
+      case 'K':
+        return 13;
+      default:
+        return 0;
     }
+  }
 
-    private Integer getValue() {
-        switch (card) {
-            case '2', '3', '4', '5', '6', '7', '8', '9':
-                return Integer.valueOf(card);
-            case 'T':
-                return 10;
-            case 'J':
-                return 11;
-            case 'Q':
-                return 12;
-            case 'K':
-                return 13;
-            default:
-                return 0;
-        }
-    }
-
-        private char getValueHex() {
+  private char getValueAlpha() {
             switch (card) {
-                case '2', '3', '4', '5', '6', '7', '8', '9':
-                    return card;
-                case 'T':
-                    return 'A';
-                case 'J':
-                    return 'B';
-                case 'Q':
-                    return 'C';
-                case 'K':
-                    return 'D';
-                default:
-                    return 0;
+              case '2':
+                return 'A';
+              case '3':
+                return 'B';
+              case '4':
+                return 'C';
+              case '5':
+                return 'D';
+              case '6':
+                return 'E';
+              case '7':
+                return 'F';
+              case '8':
+                return 'G';
+              case '9':
+                return 'H';
+              case 'T':
+                return 'I';
+              case 'J':
+                return 'J';
+              case 'Q':
+                return 'K';
+              case 'K':
+                return 'L';
+              case 'A':
+                return 'M';
+              default:
+                return 0;
             }
     }
 }
